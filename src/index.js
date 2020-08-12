d514e9f27b974ea095f18f9dffa725e55968b14d
AraceliAng <olguinaraceli@gmail.com>
2020-08-11 19:44:27 -0500

    importando uikit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Uikit from "uikit";
+import Icons from 'uikit/dist/js/uikit-icons';
+import 'uikit/dist/css/uikit.min.css';
 import App from './App';
+
+Uikit.use(Icons);
 
 ReactDOM.render(
   <React.StrictMode>
