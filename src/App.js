5f1800e920fc51228dbc9dcd8998a95500c5205b
AraceliAng <olguinaraceli@gmail.com>
2020-08-11 19:42:43 -0500

    browserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter, Switch, Redirect, Route} from 'react-router-dom';
+import './app/styles/main.css';
+import Home from './app/views/Home';
+import Paw from './app/views/Paw';
+import Apadrina from './app/views/Apadrina';
+import Adoption from './app/views/Adoption';
+import Subscribe from './app/views/Subscribe';
+import Contact from './app/views/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    
+      <BrowserRouter>
+        <Redirect from="/" to="/home" />
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/paw" component={Paw} />
+            <Route exact path="/apadrina" component={Apadrina} />
+            <Route exact path="/adoption" component={Adoption} />
+            <Route exact path="/subscribe" component={Subscribe} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+      </BrowserRouter>
+    
   );
 }
 
